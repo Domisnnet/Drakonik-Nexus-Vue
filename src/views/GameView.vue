9ca0c9d3a1d67336a48c565c32a4aca18c299696
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-10 13:24:58 +0000

    incluido

---
+++ src/views/GameView.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-6">
+      <h2 class="text-xl font-semibold mb-4">Game</h2>
+      <div class="space-y-6">
+        <GameBoard />
+        <SliderNavigator />
+      </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+  import GameBoard from '@/components/game/GameBoard.vue'
+  import SliderNavigator from '@/components/ui/SliderNavigator.vue'
+</script>
+
+<style scoped></style>  
