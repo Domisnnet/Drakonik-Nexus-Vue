2d37f52d100e3d67a95585d67430baf76c679368
dominique.goncalves61 <dominique.goncalves61@gmail.com>
2025-11-12 16:25:26 +0000

    alterado

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:felipecss/Shadow-Flip-Oh-Vue.git main:gh-pages
+git push -f git@github.com:DomisDev/drakonik-nexus.git main:gh-pages
 
 cd -
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/Shadow-Flip-Oh-Vue/',
+  base: '/drakonik-nexus/',
   plugins: [vue()],
   resolve: {
     alias: {
